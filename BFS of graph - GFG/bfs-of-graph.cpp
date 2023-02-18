0bd045bf94305f86f3cf3f5d308043ed09579862
vaibhav-3000 <118304741+vaibhav-3000@users.noreply.github.com>
2023-02-18 14:09:34 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  vector<int>ans;
+    void bfs(int V,vector<int>adj[]){
+        queue<int>q;
+        vector<int>vis(V,0);
+        q.push(0);
+        vis[0]=1;
+        while(q.size()){
+            auto f=q.front();
+            q.pop();
+            ans.push_back(f);
+            for(auto child:adj[f]){
+                if(!vis[child]){
+                    vis[child]=1;
+                    q.push(child);
+                }
+            }
+            
+        }
+    }
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        bfs(V,adj);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
