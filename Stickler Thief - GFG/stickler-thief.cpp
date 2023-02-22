83d940cbac4f3b11d6b2adffa8fff317d56de2dd
vaibhav-3000 <118304741+vaibhav-3000@users.noreply.github.com>
2023-02-22 11:47:35 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -14,7 +14,7 @@ class Solution
         dp[1][1]=arr[0];
         for(int i=1;i<n;i++){
             dp[i+1][0]=max(dp[i][0],dp[i][1]);
-            dp[i+1][1]=max(dp[i+1][0],dp[i][0]+arr[i]);
+            dp[i+1][1]=(dp[i][0]+arr[i]);
         }
         //for(auto e:dp)cout<<e[0]<<" ";cout<<endl<<endl;
        // for(auto e:dp)cout<<e[1]<<" ";cout<<endl<<endl;
